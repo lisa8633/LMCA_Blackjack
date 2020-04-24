bdad1a3f3ed64ee873f771f89ef3297f9dddeafb
lisa8633 <52472477+lisa8633@users.noreply.github.com>
2020-04-24 17:22:30 -0500

    Intro
    Co-Authored-By: Arjun Nagpal <arjunagpal@users.noreply.github.com>

---
+++ LMCA_finalproject/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  LMCA_finalproject
+//
+//  Created by Lisa Li on 4/24/20.
+//  Copyright © 2020 Lisa Li. All rights reserved.
+//
+
+import Foundation
---
+++ LMCA_finalproject/Deck.swift
@@ -0,0 +1,9 @@
+//
+//  Deck.swift
+//  LMCA_finalproject
+//
+//  Created by Lisa Li on 4/24/20.
+//  Copyright © 2020 Lisa Li. All rights reserved.
+//
+
+import Foundation
