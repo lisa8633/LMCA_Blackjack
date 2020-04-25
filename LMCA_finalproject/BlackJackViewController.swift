ff307acce77a09552eea762fd1b5b2af505a53ed
Arjun Nagpal <arjun.red2@gmail.com>
2020-04-25 15:28:51 -0500

    Initial Setup cont
    Added new VC files and adjusted end game seque unwinds

---
+++ LMCA_finalproject/BlackJackViewController.swift
@@ -0,0 +1,76 @@
+//
+//  BlackJackViewController.swift
+//  LMCA_finalproject
+//
+//  Created by Arjun Nagpal on 4/25/20.
+//  Copyright © 2020 Lisa Li. All rights reserved.
+//
+
+import UIKit
+
+class BlackJackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    @IBAction func SingleEndGame(_ sender: Any) {
+        let refreshAlert = UIAlertController(title: "End Game?", message: "The current game will be terminated.", preferredStyle: UIAlertController.Style.alert)
+
+        let okAction = UIAlertAction(title: "Yes", style: .default) {
+            (UIAlertAction) -> Void in
+            self.performSegue(withIdentifier: "unwindEndGame", sender: self)
+            NotificationCenter.default.removeObserver(self)
+
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+        style: .cancel)
+        
+        refreshAlert.addAction(okAction)
+        refreshAlert.addAction(cancelAction)
+        
+        present(refreshAlert, animated: true)
+    }
+    
+    @IBAction func MultiEndGame(_ sender: Any) {
+        let refreshAlert = UIAlertController(title: "End Game?", message: "The current game will be terminated.", preferredStyle: UIAlertController.Style.alert)
+
+        let okAction = UIAlertAction(title: "Yes", style: .default) {
+            (UIAlertAction) -> Void in
+            self.performSegue(withIdentifier: "unwindEndGame2", sender: self)
+            NotificationCenter.default.removeObserver(self)
+
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+        style: .cancel)
+        
+        refreshAlert.addAction(okAction)
+        refreshAlert.addAction(cancelAction)
+        
+        present(refreshAlert, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LMCA_finalproject/InitialViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LMCA_finalproject
+//
+//  Created by Lisa Li on 4/24/20.
+//  Copyright © 2020 Lisa Li. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    @IBAction func unwindEndGame(segue: UIStoryboardSegue) {
+    }
+
+
+}
+
