58a0ca4f58d30422268576662e1a1443a44fce89
lisa8633 <52472477+lisa8633@users.noreply.github.com>
2020-05-05 16:26:45 -0500

    database + landscape

--- LMCA_finalproject/MultiplayerViewController.swift
+++ LMCA_finalproject/MultiplayerViewController.swift
@@ -8,28 +8,56 @@
 
 import UIKit
 import FirebaseDatabase
+import CoreData
 
 class MultiplayerViewController: UIViewController {
+    var userName = ""
+    var userCoins = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        //core data
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Player")
+        //3
+        do {
+            let players = try managedContext.fetch(fetchRequest)
+            let player = players[0]
+            userName = (player.value(forKey: "name") as? String)!
+            userCoins = (player.value(forKey: "coins") as? Double)!
+            
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
 
-        // Do any additional setup after loading the view.
+        // Database
         let ref = Database.database().reference()
-        ref.child("one").setValue(["name": "Mike", "coins": 1000, "cards": "none"])
+        ref.child("one").setValue(["name": userName, "coins" : userCoins, "cards": "none"])
         ref.child("one/name").observeSingleEvent(of: .value, with: {(snapshot)in
-            let name = snapshot.value as? [String:Any]
+            let name = snapshot.value as? String
             print(name)
+        })
+        ref.child("one/coins").observeSingleEvent(of: .value, with: {(snapshot)in
+            let coins = snapshot.value as? Double
+            print(coins)
         })
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.landscapeRight, andRotateTo: UIInterfaceOrientation.landscapeRight)
         navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
