c44125c3edbcbcabf4bfea30bb517aa4ca5a26ab
lisa8633 <52472477+lisa8633@users.noreply.github.com>
2020-05-13 14:49:09 -0500

    dealt cards and gameplay

--- LMCA_finalproject/MultiplayerViewController.swift
+++ LMCA_finalproject/MultiplayerViewController.swift
@@ -17,8 +17,9 @@ class MultiplayerViewController: UIViewController{
     var userTurn = 0
     var turn = 0
     var user = User()
-    
+    var player2 = User()
     var dealer = Dealer()
+    var num = 0
     
     var userCardsDealt : Int!
     var dealerCardsDealt : Int!
@@ -39,13 +40,129 @@ class MultiplayerViewController: UIViewController{
         })
         ref.child("\(RoomCode.code)/\(userName)/num").observeSingleEvent(of: .value, with: {(snapshot)in
             self.userTurn = snapshot.value! as! Int
+            if self.userTurn == 3{
+                ref.child("\(RoomCode.code)/Dealer/cards").observeSingleEvent(of: .value) { (snapshot) in
+                let data = snapshot.value! as! String
+                if data != "none"{
+                    self.findDealer()
+                }
+            }
+            }
         })
-        ref.child("\(RoomCode.code)/turn").observeSingleEvent(of: .value, with: {(snapshot)in
+        
+        ref.child("\(RoomCode.code)/turn").observeSingleEvent(of: .value) { (snapshot) in
             self.turn = snapshot.value! as! Int
-            print(self.userTurn)
-        })
-        userBet = 10
+            if self.turn == 1{
+                self.dealer = Dealer()
+                    self.dealer = Dealer(card1: self.deck.deal(), card2: self.deck.deal())
+                    print(self.dealer.cards[0].getSymbol())
+                    print(self.dealer.cards[1].getSymbol())
+                    print("Dealer Hand Total \(self.dealer.getValue())")
+                    ref.child("\(RoomCode.code)/Dealer/cards").setValue("\(self.dealer.cards[0].getValue()),\(self.dealer.cards[0].getSuit());\(self.dealer.cards[1].getValue()),\(self.dealer.cards[1].getSuit())")
+                    self.turn += 1
+                    ref.child("\(RoomCode.code)/turn").setValue(self.turn)
+                    
+            }
+            else if self.turn == 4{
+                //everyone is done
+                if self.dealer.isBust(){
+                    //player wins
+                    print("Player wins")
+                    //flipCard(cardNum : 10)
+                    //userWinsRound(delay: dealerCardsDealt + 1)
+                }
+                while self.dealer.getValue() <= 16{
+                    self.dealer.addCard(card: self.deck.deal())
+                    print(self.dealer.cards[self.dealer.cards.count-1].getSymbol())
+                    var str = ""
+                    for c in self.dealer.cards{
+                        str += "\(c.getValue()),\(c.getSuit());"
+                    }
+                    let ref = Database.database().reference()
+                    ref.child("\(RoomCode.code)/Dealer/cards").setValue("\(str)")
+                    print(str)
+                    //animateCardDealer(dealer: dealer, cardNum: dealerCardsDealt)
+                    
+                    print("Dealer Hand Total \(self.dealer.getValue())")
+                    if self.dealer.isBust(){
+                        print("Player wins")
+                        //userWinsRound(delay: dealerCardsDealt + 1)
+                        //player wins
+                    }
+                }
+                //didnt bust and dealer and player still in
+                if !self.dealer.isBust() && !self.user.isBust(){
+                    if self.dealer.getValue() > self.user.getValue(){
+                        print("dealer wins")
+                        //dealerWinsRound(delay: dealerCardsDealt + 1)
+                        //dealer wins
+                    }else if self.dealer.getValue() == self.user.getValue(){
+                        print("tie")
+                        //self.endRound(delay: self.dealerCardsDealt + 1)
+                        // player ties
+                    }else{
+                        print("player wins")
+                        //userWinsRound(delay: dealerCardsDealt + 1)
+                    }
+                }
+                self.turn = 1
+                ref.child("\(RoomCode.code)/turn").setValue(self.turn)
+                self.dealtAlready = false
+                print("GAME OVER")
+                self.viewDidLoad()
+            }
+        }
 
+        
+            self.userBet = 10
+    }
+    func findDealer(){
+        let ref = Database.database().reference()
+        ref.child("\(RoomCode.code)/Dealer/cards").observeSingleEvent(of: .value) { (snapshot) in
+                   let data = snapshot.value! as! String
+                   if data != "none"{
+                       /*
+                       let start = data.firstIndex(of: ",")!
+                       let index = data.firstIndex(of: ";")!
+                       let v1:Int? = Int("\(data[..<start])")
+                       let s1:String? = String(data[data.index(after: start) ..< index])
+                       
+                       let second = data.lastIndex(of: ",")!
+                       let v2:Int? = Int("\(data[data.index(after: index) ..< second])")
+                       let s2:String? = String(data[data.index(after : second)...])
+                       let card1 = Card(value: v1!, suit: s1!)
+                       let card2 = Card(value: v2!, suit: s2!)
+                       self.dealer = Dealer(card1: card1, card2: card2)
+                       print(self.dealer.cards)
+        */
+                       if data[data.index(before: data.endIndex)] == ";"{
+                           data.dropLast()
+                       }
+                       print(data)
+                       let index = data.firstIndex(of: ";")!
+                       let sub1 = "\(data[..<index])"
+                       self.addDealer(data: sub1)
+                       var sub2 = "\(data[data.index(after: index)...])"
+                       while sub2.contains(";"){
+                           
+                           let index = data.firstIndex(of: ";")!
+                           let sub1 = "\(sub2[..<index])"
+                           self.addDealer(data: sub1)
+                           sub2 = "\(sub2[sub2.index(after: index)...])"
+                       }
+                       self.addDealer(data: sub2)
+                   }
+                   
+               }
+    }
+    func addDealer(data: String){
+        let start = data.firstIndex(of: ",")!
+        let v1:Int? = Int("\(data[..<start])")
+        let s1:String? = String(data[data.index(after:start)...])
+        print(v1)
+        print(s1)
+        let card = Card(value: v1!, suit: s1!)
+        self.dealer.addCard(card: card)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -81,13 +198,14 @@ class MultiplayerViewController: UIViewController{
     }
     func endTurn(){
         self.turn += 1
+        self.viewDidLoad()
         let ref = Database.database().reference()
         ref.child("\(RoomCode.code)/turn").setValue(turn)
         //update turn
         if user.blackjack && dealer.blackjack {
             print("tie")
             //flipCard(cardNum : 10)
-            //endRound(delay: dealerCardsDealt + 1)
+            endRound(delay: dealerCardsDealt + 1)
         }
         if user.blackjack && !dealer.blackjack {
             print("Player wins")
@@ -116,21 +234,39 @@ class MultiplayerViewController: UIViewController{
             print("player blackjack")
         }
     }
+    func endRound(delay: Int){
+        DispatchQueue.main.asyncAfter(deadline: .now() + Double(delay-2)) {
+            //self.animateWinnerDisplay(winner: "Tie!")
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(2)) {
+                self.dealtAlready = false
+                self.deck = Deck(numofDeck: 6)
+                self.deck.shuffle()
+                // Reset button text and interaction
+                /*
+                self.dealAndHit.setTitle("Deal", for: .normal)
+                self.stand.setTitle("", for: .normal)
+                self.stand.isUserInteractionEnabled = false
+                self.resetBet.isUserInteractionEnabled = true
+                self.doubleDown.setTitle("", for: .normal)
+                self.doubleDown.isUserInteractionEnabled = false
+                self.stand.alpha = 0
+                self.doubleDown.alpha = 0
+                self.splitAndInsurance.alpha = 0
+                
+                self.currentBalance += self.userBet
+                self.userBet = 0
+                self.totalBet.text = "Bet: \(self.userBet)"
+                self.balance.text = "Balance: \(self.currentBalance!)"
+                self.updatePlayerBalance(updatedBalance: self.currentBalance)
+                //self.flipAllCardBack()
+                self.returnCards()
+                */
+            }
+        }
+    }
     @IBAction func deal(_ sender: Any) {
         let ref = Database.database().reference()
-        ref.child("\(RoomCode.code)/turn").observeSingleEvent(of: .value, with: {(snapshot)in
-            self.turn = snapshot.value! as! Int
-        })
-        if turn == 1{
-            dealerDealt = true
-            dealer = Dealer(card1: deck.deal(), card2: deck.deal())
-            print(dealer.cards[0].getSymbol())
-            print(dealer.cards[1].getSymbol())
-            print("Dealer Hand Total \(dealer.getValue())")
-            ref.child("\(RoomCode.code)/Dealer/cards").setValue("\(dealer.cards[0].getSymbol()) ; \(dealer.cards[1].getSymbol())")
-            endTurn()
-        }
-        else if userBet > 0 && !dealtAlready && turn == userTurn{
+        if userBet > 0 && !dealtAlready && turn == userTurn{
             //Button stuff
             dealtAlready = true
             // Deal Cards
@@ -139,7 +275,7 @@ class MultiplayerViewController: UIViewController{
             print(user.cards[1].getSymbol())
             print("Player Hand Total \(user.getValue())")
             
-            ref.child("\(RoomCode.code)/\(userName)/cards").setValue("\(user.cards[0].getSymbol()) ; \(user.cards[1].getSymbol())")
+            ref.child("\(RoomCode.code)/\(userName)/cards").setValue("\(self.user.cards[0].getValue()),\(self.user.cards[0].getSuit());\(self.user.cards[1].getValue()),\(self.user.cards[1].getSuit())")
             
         }else if userBet > 0 && dealtAlready{
             
@@ -154,8 +290,13 @@ class MultiplayerViewController: UIViewController{
             user.addCard(card: deck.deal())
             print(user.cards[2].getSymbol())
             print("Player Hand Total \(user.getValue())")
+            var str = ""
+            for c in self.user.cards{
+                str += "\(c.getValue()),\(c.getSuit());"
+            }
+            ref.child("\(RoomCode.code)/\(userName)/cards").setValue(str)
             
-            userCardsDealt += 1
+            //userCardsDealt += 1
             //animateCardUser(user: user, cardNum: userCardsDealt)
             
             if user.isBust(){
@@ -166,10 +307,11 @@ class MultiplayerViewController: UIViewController{
             }
             }else{
                 endTurn()
-                //endRound(delay: dealerCardsDealt + 1)
+                endRound(delay: dealerCardsDealt + 1)
             }
     }
     @IBAction func stand(_ sender: Any) {
+        self.endTurn()
     }
     @IBAction func doubleUp(_ sender: Any) {
     }
